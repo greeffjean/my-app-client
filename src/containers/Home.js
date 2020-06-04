144aa21d76f3b409994e988a15a329766042b3a7
Jean <greeffjean@gmail.com>
2020-06-04 14:43:28 +0200

    configure AWS Amplify

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,4 +1,6 @@
-import React, {useState, useEffect} from "react";
+import React, { useRef, useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { API } from "aws-amplify";
 import "./Home.css";
 import GenreBar from "../components/GenreBar";
 import { AppContext } from "../libs/contextLib";
@@ -7,22 +9,44 @@ import MusicDatabase from '../libs/MuiscDatabase';
 
 export default function Home() {
 
+  /* useEffect hook*/
   useEffect(() => {
     loadHomeTracks()
   }, []);
 
-  async function loadHomeTracks() {
-    let data = [];
-    await MusicDatabase.searchPlayList(3220851222).then(response => data = response.tracks.data );
-    setTracks(data)
-    setLoading(false)
-  }
-
+  /* Hooks */
+  const file = useRef(null);
+  const [content, setContent] = useState("");
  const [isLoading, setLoading]  = useState(true);
  const [tracks, setTracks]  = useState();
 
+ /* Functions */
+ async function loadHomeTracks() {
+  let data = [];
+  await MusicDatabase.searchPlayList(3220851222).then(response => data = response.tracks.data );
+  setTracks(data)
+  setLoading(false)
+}
 
 
+async function handleSubmit(event) {
+  event.preventDefault();
+
+  try {
+    await createTrack({ content });
+  } catch (e) {
+    return [{ error: e.message }];
+  }
+}
+
+function createTrack(track) {
+  return API.post("notes", "/notes", {
+    body: track
+  });
+}
+
+
+/*  */
   return (
     <div className="Home">
       <AppContext.Provider value={{tracks, setTracks, setLoading}}>
@@ -47,3 +71,7 @@ export default function Home() {
     
   );
 }
+
+
+
+  
--- src/index.js
+++ src/index.js
@@ -13,7 +13,17 @@ Amplify.configure({
     mandatorySignIn: true,
     region: config.cognito.REGION,
     userPoolId: config.cognito.USER_POOL_ID,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
     userPoolWebClientId: config.cognito.APP_CLIENT_ID
+  },
+  API: {
+    endpoints: [
+      {
+        name: "MyApp",
+        endpoint: config.apiGateway.URL,
+        region: config.apiGateway.REGION
+      },
+    ]
   }
 });
 
