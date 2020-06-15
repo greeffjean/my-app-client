1bc46ddeb140b5681bbd671a5aaca4568df03e24
Jean <greeffjean@gmail.com>
2020-06-15 10:13:37 +0200

    Netlify setup

--- src/index.js
+++ src/index.js
@@ -10,17 +10,17 @@ import * as serviceWorker from './serviceWorker';
 Amplify.configure({
   Auth: {
     mandatorySignIn: true,
-    region: config.cognito.REGION,
-    userPoolId: config.cognito.USER_POOL_ID,
-    identityPoolId: config.cognito.IDENTITY_POOL_ID,
-    userPoolWebClientId: config.cognito.APP_CLIENT_ID
+    region: process.env.REACT_APP_REGION,
+    userPoolId: process.env.REACT_APP_USER_POOL_ID,
+    identityPoolId: process.env.REACT_APP_IDENTITY_POOL_ID,
+    userPoolWebClientId: process.env.REACT_APP_APP_CLIENT_ID
   },
   API: {
     endpoints: [
       {
         name: "MyApp",
-        endpoint: config.apiGateway.URL,
-        region: config.apiGateway.REGION
+        endpoint: process.env.REACT_APP_URL,
+        region: process.env.REACT_APP_REGION
       },
     ]
   }
